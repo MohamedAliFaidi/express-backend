86ee700be54a5796c53f6c7dee407980dd616fd0
MohamedAliFaidi <dalyyy3259@protonmail.com>
2023-11-03 11:01:13 +0100

    test

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const helmet = require('helmet');
-const cors = require('cors');
-const compression = require('compression');
-const { newProduct, getProduct, getProducts } = require('./controllers/productController');
+const express = require("express");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const cors = require("cors");
+const compression = require("compression");
+const {
+  newProduct,
+  getProduct,
+  getProducts,
+} = require("./controllers/productController");
 
 const app = express();
 
 const port = 4000;
-const dbConnect = require('./config/db');
+const dbConnect = require("./config/db");
 
 dotenv.config();
 
@@ -16,23 +20,29 @@ app.use(compression());
 
 app.use(
   cors({
-    origin: '*',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  }),
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
 );
 
 app.use(helmet());
-app.disable('x-powered-by');
+app.disable("x-powered-by");
 
 app.use(express.json());
 
 dbConnect();
 
-app.get('/api/products', getProducts);
+app.get("/", (req, res) => {
+ return  res.json({
+    rahtech: "Welcome to Rahtech api",
+ });
+});
 
-app.post('/api/products/create', newProduct);
+app.get("/api/products", getProducts);
 
-app.get('/api/products/getproduct/:id', getProduct);
+app.post("/api/products/create", newProduct);
+
+app.get("/api/products/getproduct/:id", getProduct);
 
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`);
